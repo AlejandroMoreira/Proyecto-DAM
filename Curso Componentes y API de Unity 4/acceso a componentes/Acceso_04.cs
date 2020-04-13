26e87ed54bd02cc2b75dc60f54b3a7765d8c5f21
AlejandroMoreira <50948492+AlejandroMoreira@users.noreply.github.com>
2020-04-13 20:16:59 +0200

    Add files via upload

---
+++ Curso Componentes y API de Unity 4/acceso a componentes/Acceso_04.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceso_04 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        print("masa " + rb.mass);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
